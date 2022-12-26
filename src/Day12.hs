ae31f64a9896b31a77249fb8b0521fc07409aacf
Sebastian Galkin <paraseba@gmail.com>
2022-12-26 13:03:48 -0300

    Speedup day 12

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,47 +1,24 @@
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
+module Day12 (main) where
 
-module Day12 where
-
+import Algorithm.Search (bfs)
 import Control.Lens hiding ((<|))
-import Control.Monad.Except (MonadError, runExcept, throwError)
-import Control.Monad.State.Strict (MonadState, evalStateT)
-import Data.Char (ord)
-import Data.Functor (($>))
-import Data.Sequence qualified as Seq
-import Data.Set (Set)
-import Data.Set qualified as Set
-import Data.Text (Text)
-import Data.Text qualified as T
+import Data.Char (ord, isLetter)
 import Utils
 import Vec2d
-
-type Path = [Coord]
+import Data.List.NonEmpty (NonEmpty)
+import Text.Megaparsec.Char (eol)
+import Text.Megaparsec (takeWhileP)
+import Data.Text (Text, unpack)
+import Control.Applicative.Combinators.NonEmpty (sepEndBy1)
+import Data.Foldable (toList)
 
 data Size = Size Row Col
     deriving (Show)
 
 data Point = Regular Coord | Exit Coord
-    deriving (Eq, Show)
-
-data NodeInfo = NodeInfo
-    { _niPath :: Path
-    , _niCoord :: Point
-    }
-    deriving (Show)
-makeLenses ''NodeInfo
-
-data TravelState = TravelState
-    { _tsQueue :: Seq.Seq NodeInfo
-    , _tsVisited :: Set Coord
-    }
-makeLenses ''TravelState
+    deriving (Eq, Ord, Show)
 
 type HeightMap = Vec2d Char
-
-main :: IO ()
-main = solve solve1 solve2
 
 neighbors :: Size -> Coord -> [Coord]
 neighbors (Size nRows nCols) (row, col) =
@@ -55,16 +32,15 @@ neighbors (Size nRows nCols) (row, col) =
   where
     valid (r, c) = r >= 0 && c >= 0 && r < nRows && c < nCols
 
-accessible :: HeightMap -> Size -> Coord -> [Point]
-accessible hm s current =
+accessible :: HeightMap -> Size -> Coord -> (Int -> Int -> Bool) -> [Point]
+accessible hm s current heightCondition =
     [ p | candidate <- (neighbors s current), let ch = hm ! candidate, let p = toPoint ch candidate, canGet ch
     ]
   where
     currentH = height (hm ! current)
 
     canGet :: Char -> Bool
-    canGet ch = height ch - currentH <= 1
-
+    canGet ch = heightCondition currentH (height ch) -- height ch - currentH <= 1
     toPoint :: Char -> Coord -> Point
     toPoint 'E' coord = Exit coord
     toPoint _ coord = Regular coord
@@ -77,119 +53,42 @@ accessible hm s current =
 findChar :: Char -> HeightMap -> [Coord]
 findChar c hm = fst <$> itoListOf (itraversed . filtered (== c)) hm
 
-popNode :: forall m. (MonadState TravelState m, MonadError Text m) => m NodeInfo
-popNode =
-    uses tsQueue uncons >>= \case
-        Just (node, rest) -> (tsQueue .= rest) $> node
-        Nothing -> throwError "No solutions"
-
-findPath ::
-    forall m.
-    (MonadState TravelState m, MonadError Text m) =>
-    HeightMap ->
-    m Path
-findPath hm = popNode >>= processNode hm
-
-processNode ::
-    forall m.
-    (MonadState TravelState m, MonadError Text m) =>
-    HeightMap ->
-    NodeInfo ->
-    m Path
-processNode hm node = do
-    case node ^. niCoord of
-        Exit _ -> pure $ node ^. niPath
-        Regular coord -> do
-            visited <- use tsVisited
-            if Set.member coord visited
-                then findPath hm
-                else
-                    modifying tsVisited (Set.insert coord)
-                        *> modifying tsQueue (Seq.>< newNodes coord)
-                        *> findPath hm
+search :: HeightMap -> Point -> (Point -> Bool) -> (Int -> Int -> Bool) -> Maybe [Point]
+search hm initial isExit heightCondition = bfs neighs isExit initial
   where
     size = Size (Row . numRows $ hm) (Col . numCols $ hm)
-    newNodes coord =
-        let neighs = accessible hm size coord
-         in Seq.fromList $ (NodeInfo (coord : node ^. niPath)) <$> neighs
+    neighs p = accessible hm size (point2coord p) heightCondition
 
-initState :: Coord -> TravelState
-initState startCoord =
-    TravelState (Seq.singleton startNode) Set.empty
+point2coord :: Point -> Coord
+point2coord (Regular c) = c
+point2coord (Exit c) = c
+
+mapP :: Parser (NonEmpty Text)
+mapP = sepEndBy1 lineP eol
   where
-    startNode = NodeInfo [] (Regular startCoord)
+    lineP :: Parser Text
+    lineP = takeWhileP Nothing isLetter
 
-run :: HeightMap -> TravelState -> Either Text Int
-run hm initial = length <$> runExcept (evalStateT (findPath hm) initial)
+main :: IO ()
+main = parseAndSolve 12 mapP solve1 solve2
 
-theMap :: HeightMap
--- theMap = testMap
-theMap = inputMap
-
-solve1 :: IO ()
-solve1 = putStrLn $ either T.unpack show $ run theMap (initState startCoord)
+solve1 :: NonEmpty Text -> Maybe Int
+solve1 input =
+    length <$> search hm (Regular startCoord) isExit heightCondition
   where
-    startCoord = findChar 'S' theMap ^?! folded
+    startCoord = findChar 'S' hm ^?! folded
+    isExit (Exit _) = True
+    isExit _ = False
+    heightCondition fromH toH = toH - fromH <= 1
+    hm = fromList (unpack <$> toList input)
 
-solve2 :: prev -> IO ()
-solve2 _ = putStrLn $ maybe "No solutions" show $ minimumOf (folded . _Right) res
+solve2 :: Foldable t => prev -> t Text -> Maybe Int
+solve2 _ input =
+    length <$> search hm (Regular startCoord) isExit heightCondition
   where
-    candidateStarts :: HeightMap -> [Coord]
-    candidateStarts hm = findChar 'S' hm ++ findChar 'a' hm
-    res = run theMap . initState <$> candidateStarts theMap
+    startCoord = findChar 'E' hm ^?! folded
+    isExit (Regular c) = hm Vec2d.! c == 'a'
+    isExit (Exit _) = False
+    heightCondition fromH toH = fromH - toH <= 1
+    hm = fromList (unpack <$> toList input)
 
-testMap :: HeightMap
-testMap =
-    fromList
-        [ "Sabqponm" :: String
-        , "abcryxxl"
-        , "accszExk"
-        , "acctuvwj"
-        , "abdefghi"
-        ]
-
-inputMap :: HeightMap
-inputMap =
-    fromList
-        [ "abccccccccaaaaccccaaacaccccaaaaaacccccccccccaaaccccccccccaaaaaaacccccccccccccccccccccccccccccacaaaccccccccccccccccccccccccccccccccccccccccaaaaa" :: String
-        , "abccccccccaaaaccaaaaaaaacccaaaaaacccccccccccaaaacccccccccaaaaaaaaaacccccccccccccccaaccccccccaaaaaccaaaccaacccccccccccccccccccccccccccccccaaaaaa"
-        , "abcccccccccaacccaaaaaaaaccccaaaaacccccccccccaaaacccccccaaaaaaaaaaaaaccccccccccaaaaaaccccccccaaaaaaaaaaaaaaccccccccccccccccaaaccccccccccccaaaaaa"
-        , "abcccccccccccccccaaaaaccccccaacaaccccaacccccaaacccccccaaaaaaaaaaaaaaccccccccccaaaaaaacccccccccaaaaacaaaaaaccccccccccccccccaaccccccccccccccccaaa"
-        , "abccccccccccccccccaaaaaccccccccccaaccaacccccccccccccccaaaaacaaaacacacccccaacccaaaaaaaacccccccaaaaacaaaaaaaccccccccccccccccaaacccccccccccccccaaa"
-        , "abcccccccccccccccaaaaaaccccccccccaaaaaaccccccccccccccccaaaaaaaacaaaaacaaaaaccccaaaaaaacccccccaacaacaaaaaaaaccccccccaaaaccaakcccccccccccccccccaa"
-        , "abcccccccccccccccaaaccacccccccccccaaaaaaacccccccaaaccccccaaaaaacaaaaaccaaaaaccaaaaaaccccccccccccaacaaaaaaaacccccccccaaaakkkklllcccccccccccccccc"
-        , "abcccccaaaccccccccccccccccccccccccaaaaaaacccccccaaacacccaaaaaaaaaaaaacaaaaaaccaaaaaacccccccccccccccccaaaccccccccccccaaakkkkkllllcccccccaacccccc"
-        , "abccccaaaacccccccccccccccccccccccaaaaaacccccccaccaaaaaccaaaaaaaaaaaaacaaaaccccccccaaccccccccccccccccccaaccccccccccccckkkkkkpllllccccaaaaaaccccc"
-        , "abccccaaaacccccccccccccccccaaacccaaaaaacccccccaaaaaaaacccaaaaacaaaaaacccaaaccccccccccccccccccccccccccccccccccccccccckkkkpppppplllcccaaaaacccccc"
-        , "abcccccaaaccccccccccccccccaaaacccccccaaccccccccaaaaacccccaaaccccaaacccccccccccccccccccccccccaaccccccccccccccccccjjjkkkkpppppppplllcccaaaaaacccc"
-        , "abccccccccccccccccccccccccaaaaccccccccccccccccccaaaaacccccccccccccccccccccccccccccccccccccccaaaaaccccccccccccjjjjjjkkkrppppuppplllccccaaaaacccc"
-        , "abccccccccccccccaaaccccccccaaaccccccccccccccccccaacaaccccccccccccccccccccccaaaccccccccaacccaaaaaccccccccccccjjjjjjjjrrrpuuuuuppplllcccccaaacccc"
-        , "abcccccaaccaacccaaacacccccccccccccccccccccccccacaaaaccccccccccccccccccccccaaaaaaccccaaaacccaaaaaaccaccccccccjjjrrrrrrrrruuuuuppplllmcccddcccccc"
-        , "abcccccaaaaaacaaaaaaaaccccccccccccccccccccccccaacaaaccccccccccccccccccccccaaaaaaccccaaaaaacccaaaaccaaacaaacjjjrrrrrrrrruuuxuuupqqlmmmmddddccccc"
-        , "abcccccaaaaaccaaaaaaaaccccccccccccccccccccccccaaaaaccccaacccccccccccccccccaaaaaacccccaaaacccaacccccaaaaaaacjjjrrrrtuuuuuuxxyuvqqqqmmmmmddddcccc"
-        , "abaacccaaaaaaccaaaaaccccccccccccccccccaaaaccccaaaaaaccaaaccccccccccccccccccaaaaaccccaaaaaccccccccccaaaaaaccjjjrrrtttuuuuuxxyvvvqqqqqmmmmdddcccc"
-        , "abaaccaaaaaaaaccaaaaaccccccccccccccccaaaaaaaaaaaaaaaacaaacaaaccccccccccccccaacaacaacaacaaccccccccaaaaaaaaccijjqqrtttxxxxxxyyvvvvvqqqqmmmmdddccc"
-        , "abaaccaaaaaaaacaaaaaaccccccccccccccccaaaaaaaaaaaaaaaaaaaaaaaacccccccccccccccaaacaaaccccccccccaaccaaaaaaaaaciiiqqqttxxxxxxxyyvvvvvvvqqqmmmdddccc"
-        , "abaaaccccaaccccaaaccacccccccccccccccccaaaaaaccccaacaaaaaaaaaaccaaaccccccccccaaaaaaaccccccccccaaaaaaaaaaaaaaiiiqqqtttxxxxxxyyyyyyvvvqqqmmmdddccc"
-        , "SbaaaccccaacccccccccccccccccccccccccaaaaaaaaccccaacccaaaaaaccaaaaaaccccccccccaaaaaacccccccccccaaaaacaaacaaaaiiiqqqttxxxxEzzyyyyyvvvqqqmmmdddccc"
-        , "abaaaccccccccccccccccccccccccccccccaaaaaaaaaaccccccccaaaaaaccaaaaaaccccccccccaaaaaaaaccccccccaaaaaacaaaccaaaiiiqqqtttxxxyyyyyyvvvvqqqmmmdddcccc"
-        , "abaccccccaacccccccccccccccccccccccccaaaaaaaaaaaacccccaaaaaaacaaaaaacccccccccaaaaaaaaacccccccaaaaaaaacaaaaaaaiiiqqqtttxxyyyyyyvvvvqqqqnnmeddcccc"
-        , "abccccccaaaaccccccccccccaaaccccccccccccaaaaaaaaaaacccaaacaaacaaaaacccccccccaaaaaaaaaacccccccaaaaaaaaccaaaaaaaiiiqqtttxxyyyyyywvrrrrnnnneeeccccc"
-        , "abccccccaaaacccccaacccccaaaacccccccccccaaaccaaaaaacccacccccccaaaaacccccccccaaacaaacccccccccccccaacccccccaaaaaiiqqqttxxwywwyyywwrrrnnnneeeeccccc"
-        , "abccccccaaaaccaacaaaccccaaaaccccccaacccaacccaaaaaccccccccccccccccccccccccccccccaaacccccccccccccaaccccccaaaaaaiiqqqttwwwwwwwwywwrrrnnneeeecccccc"
-        , "abccccccccccccaaaaacccccaaaccccacaaacccccccccaaaaacccccccccccccccccccccccccccccaaacccccccccccccccccccccaaaaaaiiqqpsswwwwsswwwwwrrnnneeeeccccccc"
-        , "abcccccccccccccaaaaaacccccccccaaaaacaacccccccaacaacccccaaccccccccccccccccccccccccccccccccccccccccccccccaccaahhhpppssssssssswwwwrrnnneeeaccccccc"
-        , "abcccccccccccaaaaaaaacccccccccaaaaaaaacccccaaccccccaaacaaccccccccccccccccccccccccccccccccccccaaaccaccccccccchhhpppsssssssssswwrrrnnneeeaaaacccc"
-        , "abcccccccccccaaaaacaacccccccccccaaaaaccaaaaaaccccccaaaaaaccccccccccccccccccccccccccccaaccaaccaaaaaacccccccccchhpppppsspppossrrrrrnnneeeaaaacccc"
-        , "abccccccccccccacaaaccccccccccccaaaaacccaaaaaaaacccccaaaaaaaccaaaccccccccaaaacccccccccaaaaaacccaaaaacccccccccchhhpppppppppoosrrrroonffeaaaaacccc"
-        , "abccccccccccccccaaaccccccccccccaacaaaccaaaaaaaaccccaaaaaaaaacaaaccccccccaaaacccccccccaaaaaccaaaaaaacccccccccchhhhpppppppoooooooooonfffaaaaccccc"
-        , "abcccccccccccccccccccccccaaaccccccaaccccaaaaaaaccccaaaaaaaaaaaaaaaccccccaaaacccccccccaaaaaacaaaaaaaacccccaacchhhhhhhhgggggoooooooofffaaaaaacccc"
-        , "abcccccccccccccccccccccccaaaaacccaacccccaaaaaccccccaaaaaacaaaaaaaaccccccaaacccccccccaaaaaaaaaaaaaaaaccccaaacccchhhhhgggggggooooooffffaaaaaacccc"
-        , "abccaaacccccccccccccccccaaaaaaccaaaccccaaaaaacccccccccaaacccaaaaacccccccccccccccccccaaaaaaaaccaaacacccccaaacaaachhhggggggggggfffffffcaacccccccc"
-        , "abcaaaaccccccccccaacccccaaaaaaccaaacaaaccccaaccccccccccccccaaaaacccccccccccccccccccccccaacccccaaaccccaaaaaaaaaacccccccccaagggffffffcccccccccccc"
-        , "abcaaaaccccccccccaaccccccaaaaaaaaaaaaaaccccccccccccccccccccaaaaaaccccccccccccccccccccccaaccccccccccccaaaaaaaaaccccccccccaaacgfffffcccccccccccaa"
-        , "abccaaacccccccaaaaaaaacccaaaaaaaaaaaaaaccccccccaaaaaccaaaaaaaaaaaaaacaacccccccccaaaccacccccccccccccccccaaaaacccccccccccaaaaccccccccccccccccccaa"
-        , "abccccccccccccaaaaaaaacccccccccaaaaaaccccccccccaaaaaccaaaaaaaaaaaaaacaaaaaacccccaaaaaacccccccccccccccccaaaaaacccccccccccaacccccccccccccccacacaa"
-        , "abccccccccccccccaaaacccccccccccaaaaaacccccccccaaaaaacccaaaaaaaaaaaaacaaaaaacccccaaaaaacccccccccccccccccaaaaaaaccccccccccaacccccccccccccccaaaaaa"
-        , "abcccccccccccccaaaaaaccccccccccaaaaaaaccccccccaaaaaaccccccaaaaaacccaaaaaaaaccccaaaaaaaaccccccccccccccccaaacaaacccccccccccccccccccccccccccaaaaaa"
-        ]
